0ae3e6a90cd415625f9a426da744d262a9cf3f45
Sahaj Thakkar <62639536+sahaj18@users.noreply.github.com>
2023-12-17 17:00:34 +0530

    Add files via upload

---
+++ Common/footer.php
@@ -0,0 +1,44 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3> UPG College</h3>
+            <p>This Project has been made by Raj Vadhan, Sahaj Thakkar, and Aesha Shah.
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About UPG</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.M.S(Business Management System)</a></li>
+              <li><a href="">B.M.M(Bachelor of Mass Media)</a></li>
+              <li><a href="">B.Sc.IT(Bachelor in Information Technology)</a></li>
+              <li><a href="">B.A(Multimedia and Mass communication)</a></li>
+              <li><a href="">MSc.IT(Master in Information Technology)</a></li>
+              <li><a href="">M COM(Master of Commerce</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Usha Pravin Gandhi College of Arts, Science and Commerce,
+                 Bhakti Vedanta,Swami Marg,Juhu Scheme, Vile Parle(West),
+                 Mumbai-400056,
+                 Maharashtra,
+                 India</p>
+              <p>Telephones: 42332041-44</p>
+              <p>E-mail: info@upgcm.ac.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights ©</p>
+</footer>
---
+++ academics.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Academics</title>
+</head>
+<body>
+	<?php include('common/header.php') ?>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Usha Pravin Gandhi College Arts, Science and Commerce</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Vision and Mission</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+						Vision
+The institution strives to empower students with knowledge and skills in their chosen fields, by providing opportunities to realize their potential by motivating them towards community linked initiatives, thereby shaping them into future leaders.. ·
+
+ 
+
+Mission
+To inculcate life skills by providing value-based education and nurturing a scientific spirit of inquiry in the young minds.
+ 
+
+ 
+
+Objectives
+
+To sharpen students’ focus and inculcate research culture thereby helping them to achieve academic excellence.
+To empower them for lifelong learning by fostering rational and independent thinking.
+To sensitise the students towards the neighbouring environment and society at large.
+To stimulate students’ inborn talent and skill by understanding their aptitude and capability and to mould their passion into profession.
+Quality Policy
+
+Ensuring the support of all stakeholders enables the college to improve the services offered to the learners.
+Effective utilization of human and other resources to enhance the quality of education.
+Providing state-of-the-art infrastructure and ICT facilities to augment the teaching-learning process.
+Fostering competencies to equip learners to face demands of a changing world.
+Inculcating a sense of social and moral responsibilities towards society and the environment.
+						</p>
+					
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Principal DR. ANJU KOOPUR.</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+						Dear Students, Parents and Teachers,
+
+Society is changing rapidly in many ways which will have a profound impact on the youth. However, in this changing world, knowledge will remain a key resource around the globe. The dynamics of the fast-changing world urges the academicians to give the right impetus for making life-long learners out of students. At Usha Pravin Gandhi College the key focus has been to impart professional education whilst keeping pace with a world that is quite dynamic and competitive. The academic staff at the college leaves no stone unturned in making our student community ready and equipped with the required abilities to interpret and utilize newer technology in all walks of life.
+
+I welcome all the students who chose to be a part of SVKM’s Usha Pravin Gandhi College of Arts, Science and Commerce in the new academic year. I am sure that you will feel proud to be associated with us and make us equally proud with your academic excellence and accomplishments in the future.
+
+My best wishes to all at Usha Pravin Gandhi College of Arts, Science and Commerce at the start of 2020-2022.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of B.Sc.IT(Bachelor in Information Technology)</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Dr. Neelam Naik, Asst.Prof</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+						Ph.D. in Computer Science from Department of Computer Science, University of Mumbai.
+
+M.Phil. in Computer Science from Yashwantrao Chanvan Maharashtra Open University
+
+Master of Computer Applications, Indira Gandhi National Open University, New Delhi
+
+ 
+
+Working as an Assistant Professor for last 12 years (degree and masters level) with reputed institutes under University of Mumbai and SNDT Univeristy, she has gained valuable experience in teaching and supervising post graduate students. In addition to role of classroom instructor, she has served as a project advisor for students in their graduates and post graduates research  and software projects, where her job was to assign them appropriate topics, edit and evaluate their work.
+
+ 
+
+She has published papers in peer reviewed international conferences and journals. Her research interest areas are Machine Learning, Data Mining, Data Science and Big and Massive Data Analytics.
+
+ 
+
+Committee Member: Research Committee, Women Development Cell, Attendance Committee, Website Committee
+
+Email id: neelam.naik@upgcm.ac.in
+
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of B.Sc.IT(Bachelor in Information Technology) </h4>
+			</div>
+			<div class="container">
+				<div class="row"> 
+					<h4 class="dean-message-heading mt-3">Dr. Swapnali Lotlikar, Asst.Prof(Co-ordinator BSc.IT)</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+						Dr. Mrs. Swapnali Lotlikar has completed her M.Phil(Computer Science) from Madurai University in 2009.She has completed her Masters In Computer Management from Pune University.She has over 8 years of teaching experience for the Bachelors of Information Technology . She has joined as a lecturer in the B.Sc(I.T.) department of Usha Pravin Gandhi College of Management in June 2006.She was earlier associated with K.C. College for 4 years.
+
+Committee Member of: Examination Committee/Unfair Means Inquiry Committee, College Women Development Cell, DLLE, Cultural Committee,
+
+E-mail: swapnali.lotlikar@upgcm.ac.in
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
